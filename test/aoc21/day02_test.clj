fcffbc3186771824fee02632fc35cf9f1c47c36b
Bob Follek <bfollek@gmail.com>
2021-12-02 13:25:08 -0500

    Added test for direction exception

--- test/aoc21/day02_test.clj
+++ test/aoc21/day02_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [aoc21.day02 :refer :all]))
 
-(deftest day01
+(deftest day02
   (testing "part-1."
     (is (= 1990000 (aoc21.day02/part-1 "data/day02.txt")))))
+
+(deftest direction-exception
+  (testing "direction exception."
+    (is (thrown-with-msg? Exception #"Unknown direction" (aoc21.day02/part-1 "data/day02-direction-exception.txt")))))
