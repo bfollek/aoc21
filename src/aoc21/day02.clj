583394b617ffa7625f369c6afbab40c0c097150e
Bob Follek <bfollek@gmail.com>
2021-12-02 12:57:56 -0500

    Day 2 part 1 done

--- src/aoc21/day02.clj
+++ src/aoc21/day02.clj
@@ -5,6 +5,8 @@
 
 (defrecord Move [direction distance])
 
+(defrecord Position [horizontal depth])
+
 (defn parse-move
   [s]
   (let [[direction distance] (str/split s #" ")]
@@ -16,4 +18,24 @@
    file-name
    rh/read-lines
    (map parse-move)))
-   ;(map rh/to-int)))
+
+(defn make-move
+  [pos move]
+  (case (:direction move)
+    "forward" (assoc pos :horizontal (+ (:horizontal pos) (:distance move)))
+    "up" (assoc pos :depth (- (:depth pos) (:distance move)))
+    "down" (assoc pos :depth (+ (:depth pos) (:distance move)))))
+  ;;TODO
+  ;;(def specific-watson (assoc watson :appears-in "Sign of the Four"))
+;;    (case mystr
+;;     "" 0
+;;     "hello" (count mystr)))
+
+
+;; 1990000
+(defn part-1
+  "Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?"
+  [file-name]
+  (let [moves (load-moves file-name)
+        pos (reduce make-move (->Position 0 0) moves)]
+    (* (:horizontal pos) (:depth pos))))
