07cf804ea862467d2798ccbb637f9ff8f1590eaf
Bob Follek <bfollek@gmail.com>
2021-12-02 21:06:08 -0500

    Set up for day 3

--- test/aoc21/day03_test.clj
+++ test/aoc21/day03_test.clj
@@ -4,6 +4,6 @@
 
 (deftest day03
   (testing "part-1."
-    (is (= 1 (aoc21.day03/part-1 "data/day03.txt"))))
+    (is (= nil (aoc21.day03/part-1 "data/day03.txt"))))
   (testing "part-2."
-    (is (= 2 (aoc21.day03/part-2 "data/day03.txt")))))
+    (is (= nil (aoc21.day03/part-2 "data/day03.txt")))))
