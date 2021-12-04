07cae41d188e116f1fed1e44540bd78b827fc095
Bob Follek <bfollek@gmail.com>
2021-12-03 19:27:31 -0500

    WIP

--- src/aoc21/day03.clj
+++ src/aoc21/day03.clj
@@ -2,26 +2,26 @@
   (:require
    [rabbithole.core :as rh]))
 
-(defn count-one-bits
+(defn one-bits
   [counters diagnostic]
   (map #(+ %1 (if (= %2 \1) 1 0)) counters diagnostic))
 
-(defn count-most-common-bits
+(defn most-common-bits
   [diagnostics]
   (let [counters (into (vector-of :int) (repeat (count (nth diagnostics 0)) 0))
-        counters     (reduce count-one-bits counters diagnostics)]
-    counters))
+        counters     (reduce one-bits counters diagnostics)]
+    (map #(if (> %1 (/ (count diagnostics) 2)) 1 0) counters)))
 
 (defn part-1
   [file-name]
   (let [diagnostics (rh/read-lines file-name)
-        counters (count-most-common-bits diagnostics)]
+        mcb (most-common-bits diagnostics)]
         ;; Set up counters for each digit, and init them to 0.
         ;; counters  (into (vector-of :int) (repeat (count (nth diagnostics 0)) 0))]
         ;; (->>
         ;; counters
         ;; (count-most-common-bits diagnostics))
-    counters))
+    mcb))
 
 (defn part-2
   [file-name])
