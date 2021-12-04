ed02a5069fb2d456c51f3f0cf4741df1bf75174c
Bob Follek <bfollek@gmail.com>
2021-12-03 19:37:49 -0500

    WIP

--- src/aoc21/day03.clj
+++ src/aoc21/day03.clj
@@ -1,26 +1,28 @@
+
 (ns aoc21.day03
   (:require
    [rabbithole.core :as rh]))
 
 (defn one-bits
   [counters diagnostic]
+  ;; If the nth diagnostic bit is 1, add 1 to the nth counter, else 0.
   (map #(+ %1 (if (= %2 \1) 1 0)) counters diagnostic))
 
 (defn most-common-bits
   [diagnostics]
-  (let [counters (into (vector-of :int) (repeat (count (nth diagnostics 0)) 0))
-        counters     (reduce one-bits counters diagnostics)]
+  ;; Start with a vector of zeros, one for each bit in a `diagnostic`.
+  (as-> (into (vector-of :int) (repeat (count (nth diagnostics 0)) 0)) counters
+    (reduce one-bits counters diagnostics)
     (map #(if (> %1 (/ (count diagnostics) 2)) 1 0) counters)))
+
+;;   (let [counters (into (vector-of :int) (repeat (count (nth diagnostics 0)) 0))
+;;         counters     (reduce one-bits counters diagnostics)]
+;;     (map #(if (> %1 (/ (count diagnostics) 2)) 1 0) counters)))
 
 (defn part-1
   [file-name]
   (let [diagnostics (rh/read-lines file-name)
         mcb (most-common-bits diagnostics)]
-        ;; Set up counters for each digit, and init them to 0.
-        ;; counters  (into (vector-of :int) (repeat (count (nth diagnostics 0)) 0))]
-        ;; (->>
-        ;; counters
-        ;; (count-most-common-bits diagnostics))
     mcb))
 
 (defn part-2
