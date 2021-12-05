97bd2de03ab89fa391a4b04ba649bce2bed36c4d
Bob Follek <bfollek@gmail.com>
2021-12-05 03:15:29 -0500

    Day 3 part 2 - Changed exception to ArrayIndexOutOfBoundsException. ATP.

--- src/aoc21/day03.clj
+++ src/aoc21/day03.clj
@@ -44,7 +44,7 @@
     (let [[mcb lcb] (common-bits diags)
           bits (if use-most-common-bits? mcb lcb)]
       (when (> index (count bits))
-        (throw (IllegalArgumentException. "index > bits size:")))
+        (throw (ArrayIndexOutOfBoundsException. "index > bits size:")))
       (if (<= (count diags) 1)
         (bit-seq-to-decimal (first diags))
         (recur (check-bit diags bits index) (inc index))))))
