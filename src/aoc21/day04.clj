8cc7df669c71b99a9d004f672e18d7f62f513952
Bob Follek <bfollek@gmail.com>
2021-12-07 21:47:08 -0500

    Started day 4.

--- src/aoc21/day04.clj
+++ src/aoc21/day04.clj
@@ -1,14 +1,20 @@
 (ns aoc21.day04
   (:require
+   [clojure.string :as str]
    [rabbithole.core :as rh]))
+
+(def board-size 5)
 
 (defrecord Bingo [numbers boards])
 
 (defn load-game
   [file-name]
-  (->>
-   file-name
-   rh/read-lines))
+  (let [lines (rh/read-lines file-name)
+        numbers (str/split (nth lines 0) #",")
+        ;; Skip the `numbers` line, and ignore the blank lines.
+        board-lines (filter #(not (str/blank? %1)) (drop 1 lines))
+        boards (partition board-size board-lines)]
+    (->Bingo numbers boards)))
 
 (defn part-1
   [file-name]
